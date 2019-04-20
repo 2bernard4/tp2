5664137545c26ae9759be2b4f23d0a0fac3f7a14
bernardo <chilettobernardo@gmail.com>
2019-04-20 03:09:48 -0300

    tercer commit

--- src/main/java/bernardochiletto/bdd/Jbdc.java
+++ src/main/java/bernardochiletto/bdd/Jbdc.java
@@ -69,7 +69,6 @@ public class Jbdc {
         Connection conn = null;
         Statement stmt = null;
         java.util.Date dt = new java.util.Date();
-
         java.text.SimpleDateFormat sdf =
                 new java.text.SimpleDateFormat("yyyy-MM-dd");
 
